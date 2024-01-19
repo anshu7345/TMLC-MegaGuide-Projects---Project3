6d516f55cc8639d76565244386757dfc8a26bd2f
anshu7345 <anshu.singcoc@gmail.com>
2024-01-19 14:20:59 +0530

    save

--- blueberryyeildpridect.py
+++ blueberryyeildpridect.py
@@ -1,19 +1,19 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import joblib
 from PIL import Image
-
 import os
 
 # Get the absolute path to the directory where your models are stored
-models_directory = os.path.join(os.getcwd(), 'https://github.com/anshu7345/TMLC-MegaGuide-Projects---Project3/tree/main')
+models_directory = os.path.join(os.getcwd(), 'https://github.com/anshu7345/TMLC-MegaGuide-Projects---Project3')  # Update 'models' to your actual directory name
 
 # Load models and objects
 best_model_path = os.path.join(models_directory, 'lreg_bbry_tuned_model.pkl')
 rf_final_path = os.path.join(models_directory, 'rf_bbry_tuned_model.pkl')
 fs_rf_path = os.path.join(models_directory, 'rf_fs_object.joblib')
+
+# Load models using joblib.load
+best_model = joblib.load(best_model_path)
+rf_final = joblib.load(rf_final_path)
 
 # Define Streamlit app
 def main():
@@ -45,14 +45,14 @@ def display_predictions(user_input):
     st.header("Model Predictions")
 
     try:
-        if best_model_path is not None:
-            prediction_best_model = best_model_path.predict([list(user_input.values())])
+        if best_model is not None:
+            prediction_best_model = best_model.predict([list(user_input.values())])
             st.write(f"Best Model Prediction: {prediction_best_model[0]}")
         else:
             st.error("Best model is not loaded.")
 
-        if rf_final_path is not None:
-            prediction_rf_final = rf_final_path.predict([list(user_input.values())])
+        if rf_final is not None:
+            prediction_rf_final = rf_final.predict([list(user_input.values())])
             st.write(f"Random Forest Prediction: {prediction_rf_final[0]}")
         else:
             st.error("Random Forest model is not loaded.")
@@ -60,13 +60,12 @@ def display_predictions(user_input):
     except Exception as e:
         st.error(f"Error during prediction: {e}")
 
-
 def display_shap_force_plot():
     # Display SHAP force plot from a URL
     st.header("SHAP Force Plot")
 
     # URL to the image
-    image_url = 'https://github.com/anshu7345/TMLC-MegaGuide-Projects---Project3/blob/main/test_force_plot1.png'
+    image_url = 'https://github.com/anshu7345/TMLC-MegaGuide-Projects---Project3/raw/main/test_force_plot1.png'
 
     # Display the image directly using st.image
     st.image(image_url, caption='SHAP Force Plot', use_column_width=True)
